6941777cfcd75b490e272e53f9c5462fb163cf4e
Tadeu Valentt <tadeu.valentt@ipanemax.com>
2014-09-02 08:37:09 -0300

    change account settings

--- www/js/PushwooshAndroid.js
+++ www/js/PushwooshAndroid.js
@@ -48,7 +48,7 @@ function registerPushwooshAndroid() {
 	//!!! Please note this is an API for PGB plugin. This code is different in CLI plugin!!!
 	//At the moment I cannot update the plugin to the latest version. TY PGB Team!
 	//see http://community.phonegap.com/nitobi/topics/malformed_xml_in_plugin_xml_file?utm_source=notification&utm_medium=email&utm_campaign=new_reply&utm_content=reply_button&reply%5Bid%5D=14224918#reply_14224918
-	pushNotification.registerDevice({ projectid: "t4deu-valentt", appid : "0EF2C-CCCFD" },
+	pushNotification.registerDevice({ projectid: "509541969118", appid : "0EF2C-CCCFD" },
 		function(token)
 		{
 			alert(token);
